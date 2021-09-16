400c447b429081ee315074098f66f0d0cc396a05
Akilah <akilahrabb@gmail.com>
2021-09-16 13:54:45 -0400

    deleted rts folder and amended ConcatTwoArraysPractice.java

--- src/test/java/com/Practice/InterviewQuestionPractice/ConcatTwoArraysPractice.java
+++ src/test/java/com/Practice/InterviewQuestionPractice/ConcatTwoArraysPractice.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class ConcatTwoArraysPractice {
 
-    public static int[] concatTwoArrays (int[] arr1, int[] arr2, int[] arr3){
+    public static int[] concatTwoArrays (int[] arr1, int[] arr2){
 
-        int[] concat = new int[arr1.length+ arr2.length + arr3.length];
+        int[] concat = new int[arr1.length+ arr2.length];
 
         int i = 0;
 
@@ -23,10 +23,6 @@ public class ConcatTwoArraysPractice {
         // adding to the concat array at concat's index i=[arr1.length+1].
         // [arr1[0], arr[1], ..., arr1[arr1.length], arr2[0], arr2[1], ..., arr2[arr2.length]]
 
-        for(int each: arr3){
-            concat[i] += each;
-            i++;
-        }
         return concat;
     }
 
@@ -34,8 +30,7 @@ public class ConcatTwoArraysPractice {
 
         int[] array1 = {1,2,3,4,5};
         int[] array2 = {7,8,9};
-        int[] array3 = {101, 102, 103, 104, 105, 987};
 
-        System.out.println(Arrays.toString(concatTwoArrays(array1, array2, array3)));
+        System.out.println(Arrays.toString(concatTwoArrays(array1, array2)));
     }
 }
